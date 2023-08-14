03718bafe9fe3384fabbe88a76bd50c4a8428969
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-08-14 10:17:34 -0300

    logica do hangman concluida

--- hangman.py
+++ hangman.py
@@ -1,5 +1,6 @@
 import random
 from words import words
+from img_hangman import HANGMAN
 
 def get_valid_word(words):
     word=random.choice(words)
@@ -9,7 +10,7 @@ def get_valid_word(words):
     return word.upper()
 
 
-word=get_valid_word(words)
+word='CACHORRO'
 print(word)
 
 word_letter_list=[word for word in word]
@@ -17,3 +18,21 @@ print(word_letter_list)
 
 guessed_letter_list=['_' for word in word]
 print(guessed_letter_list)
+
+print()
+
+tentativas=0
+while True:
+    letter=input('Tente uma letra: ').upper()
+    if len(letter)==1:
+        while letter in word_letter_list:
+            index=word_letter_list.index(letter)
+            word_letter_list[index]='*'
+            guessed_letter_list[index]=letter
+    else:
+        print('Entrada de dados deve ser feita com apenas um caractere por favor.\n')
+    tentativas+=1
+    
+    print(guessed_letter_list)
+    print(word_letter_list)
+    print('Número de tentativas efetuadas: \n'.format(tentativas))
